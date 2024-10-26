175ba94b0e6379e1234c507e635ce5f4fcad70cc
AngelTheAnalyst <angelawilliam262@gmail.com>
2024-10-26 18:34:34 +0300

    Add files via upload

---
+++ backend api/api/reviews.route.js
@@ -0,0 +1,21 @@
+{
+    "type"; "module",
+    "dependencies"; {
+      "express"; "^4.18.2"  // Example dependency
+    }
+  }
+  
+  
+  import express from "express"
+import ReviewsCtrl from "./reviews.controller.js"
+
+const router = express.Router()
+
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
+router.route("/new").post(ReviewsCtrl.apiPostReview)
+router.route("/:id")
+    .get(ReviewsCtrl.apiGetReview)
+    .put(ReviewsCtrl.apiUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
+
+export default router
